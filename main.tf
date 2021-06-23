89aee27d1f584aaa8581f6dbc6976aad22d23758
angelrz798 <rzrivera_angel@outlook.com>
2021-06-23 18:11:16 +0000

    Add new Terraform configuration for EC2

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "angel.ruiz"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "jenkins_ssh_agent" {
+  ami           = "ami-830c94e3"
+  instance_type = "t3a.medium"
+
+  tags = {
+    Name = "jenkins-ssh-agent"
+  }
+}
+
